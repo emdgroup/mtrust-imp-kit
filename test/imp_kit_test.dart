170aa61e0f9821599e4afa4107a39ad87261dfa9
JoshuaWellbrock <info@joshuawellbrock.com>
2025-07-22 14:57:23 +0200

    chore: make integration test conditional

--- test/imp_kit_test.dart
+++ test/imp_kit_test.dart
@@ -4,46 +4,53 @@ import 'package:mtrust_urp_wifi_strategy/mtrust_urp_wifi_strategy.dart';
 
 void main() {
 
-  final strategy = UrpWifiStrategy();
-  final reader = ImpReader(connectionStrategy: strategy);
+  const runTest = bool.fromEnvironment('run_test', defaultValue: false);
 
-  group('Firmware Compatibility Check', () {
-    test('Test that the Kit can connect to a reader', () async {
-      final res = await strategy.findAndConnectDevice(
-        deviceAddress: 'localhost:8080',
-      );
-      expect(res, true);
+  // NOTE: Run this test only if the device simulator is running
+  // The firmware compatibility check is using this test to check compatibility
+  if(runTest) {
+    final strategy = UrpWifiStrategy();
+    final reader = ImpReader(connectionStrategy: strategy);
+
+    group('Firmware Compatibility Check', () {
+      test('Test that the Kit can connect to a reader', () async {
+        final res = await strategy.findAndConnectDevice(
+          deviceAddress: 'localhost:8080',
+        );
+        expect(res, true);
+      });
+
+      test('Get Device Info', () async {
+        final res = await reader.info();
+        expect(res, isA<UrpDeviceInfo>());
+      });
+
+      test('Get Public Key', () async {
+        final res = await reader.getPublicKey();
+        expect(res, isA<UrpPublicKey>());
+      });
+
+      test('Get Name', () async {
+        final res = await reader.getName();
+        expect(res, isA<UrpDeviceName>());
+      });
+
+      test('Get Device ID', () async {
+        final res = await reader.getDeviceId();
+        expect(res, isA<UrpDeviceId>());
+      });
+
+      test('Get Prime Reader', () async {
+        final res = await reader.prime();
+        expect(res, isA<UrpImpPrimeResponse>());
+      });
+
+      test('Perform measurement', () async {
+        final res = await reader.startMeasurement();
+        expect(res, isA<UrpImpSecureMeasurement>());
+      });
+
     });
-
-    test('Get Device Info', () async {
-      final res = await reader.info();
-      expect(res, isA<UrpDeviceInfo>());
-    });
-
-    test('Get Public Key', () async {
-      final res = await reader.getPublicKey();
-      expect(res, isA<UrpPublicKey>());
-    });
-
-    test('Get Name', () async {
-      final res = await reader.getName();
-      expect(res, isA<UrpDeviceName>());
-    });
-
-    test('Get Device ID', () async {
-      final res = await reader.getDeviceId();
-      expect(res, isA<UrpDeviceId>());
-    });
-
-    test('Get Prime Reader', () async {
-      final res = await reader.prime();
-      expect(res, isA<UrpImpPrimeResponse>());
-    });
-
-    test('Perform measurement', () async {
-      final res = await reader.startMeasurement();
-      expect(res, isA<UrpImpSecureMeasurement>());
-    });
-
-  });
+  }
+  
 }
